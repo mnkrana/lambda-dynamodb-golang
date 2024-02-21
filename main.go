b6b99f3c87f0f985578a48a99e68f3de481a777d
Mayank <mayank@mithyagames.com>
2024-02-21 19:54:44 +0530

    feat(delete): add delete by key and value

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+	"github.com/aws/aws-sdk-go/service/dynamodb/expression"
 	"github.com/google/uuid"
 )
 
@@ -31,5 +32,44 @@ func PutNewItem(item string) {
 	_, err := dynamodbSession.PutItem(input)
 	if err != nil {
 		log.Printf("Error in puting item %v", err)
+	}
+}
+
+func DeleteItemByKeyValue(key string, value string) {
+	filt := expression.Name(key).Equal(expression.Value(value))
+
+	expr, err := expression.NewBuilder().WithFilter(filt).Build()
+	if err != nil {
+		log.Fatalf("Got error building expression: %s", err)
+	}
+
+	params := &dynamodb.ScanInput{
+		ExpressionAttributeNames:  expr.Names(),
+		ExpressionAttributeValues: expr.Values(),
+		FilterExpression:          expr.Filter(),
+		TableName:                 aws.String(TABLE),
+	}
+
+	result, err := dynamodbSession.Scan(params)
+	if err != nil {
+		log.Fatalf("Query API call failed: %s", err)
+	}
+
+	//NOTE: delete only first found item at 0 index
+	input := &dynamodb.DeleteItemInput{
+		Key: map[string]*dynamodb.AttributeValue{
+			"uuid": {
+				S: aws.String(*result.Items[0]["uuid"].S),
+			},
+		},
+		TableName: aws.String(TABLE),
+	}
+
+	_, err = dynamodbSession.DeleteItem(input)
+
+	if err != nil {
+		log.Printf("Error in deleting item %v", err)
+	} else {
+		log.Printf("Successfully deleted item")
 	}
 }
