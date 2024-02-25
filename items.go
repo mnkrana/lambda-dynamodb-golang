c008028bdb19730773ef10fb9464b7f5db09827e
Mayank <mayank@mithyagames.com>
2024-02-25 15:01:52 +0530

    feat(state): add new state for player index

--- items.go
+++ items.go
@@ -13,11 +13,23 @@ func (d State) EnumIndex() int {
 	return int(d)
 }
 
+type Player int
+
+const (
+	Me Player = iota + 1
+	Friend
+)
+
+func (p Player) EnumIndex() int {
+	return int(p)
+}
+
 const (
 	KEY_UUID               = "uuid"
 	KEY_MyConnectionID     = "myConnectionID"
 	KEY_FriendConnectionID = "friendConnectionID"
 	KEY_State              = "clientState"
+	KEY_Player             = "player"
 )
 
 type ConnectionItem struct {
@@ -25,4 +37,5 @@ type ConnectionItem struct {
 	MyConnectionID     string `json:"myConnectionID"`
 	FriendConnectionID string `json:"friendConnectionID"`
 	State              int    `json:"clientState"`
+	Player             int    `json:"player"`
 }
