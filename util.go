fca1524ed495a214a33e467f7b355d19a258bd36
Mayank <mayank@mithyagames.com>
2024-03-19 13:29:23 +0530

    feat(player): get total

--- util.go
+++ util.go
@@ -38,6 +38,35 @@ func findItemByKeyValue(key string, value string) ConnectionItem {
 	return getConnectionItemFromResult(result)
 }
 
+func GetTotal(key string, value int) ConnectionItem {
+	filt := expression.Name(key).GreaterThan(expression.Value(value))
+
+	expr, err := expression.NewBuilder().WithFilter(filt).Build()
+	if err != nil {
+		log.Fatalf("Got error building expression: %s", err)
+	}
+
+	params := &dynamodb.ScanInput{
+		ExpressionAttributeNames:  expr.Names(),
+		ExpressionAttributeValues: expr.Values(),
+		FilterExpression:          expr.Filter(),
+		TableName:                 aws.String(TABLE),
+	}
+
+	result, err := dynamodbSession.Scan(params)
+	if err != nil {
+		log.Printf("Query API call failed: %s", err)
+	}
+
+	if *result.Count == 0 {
+		log.Println("No key found")
+		return ConnectionItem{}
+	}
+
+	//return first find
+	return getConnectionItemFromResult(result)
+}
+
 func getConnectionItemFromResult(result *dynamodb.ScanOutput) ConnectionItem {
 	state, err := strconv.Atoi(*result.Items[0][KEY_State].N)
 	if err != nil {
